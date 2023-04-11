a57e15cc269b9672e66cd4f145f0ff4a65195242
ardarika <mozartneverdie@gmail.com>
2023-04-11 19:47:51 +0300

    add hw1

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def blogs(request):
+    return HttpResponse('home page')
+
+
+def about(request):
+    return HttpResponse('about')
+
+
+def comment(request):
+    return HttpResponse('leave your comment')
+
+
+def create(request):
+    return HttpResponse('create your post')
+
+
+def update(request):
+    return HttpResponse('update')
+
+
+def delete(request):
+    return HttpResponse('delete')
+
+
+def profile(request):
+    return HttpResponse('profile')
+
+
+def register(request):
+    return HttpResponse('register')
+
+
+def login(request):
+    return HttpResponse('login')
+
+
+def change_password(request):
+    return HttpResponse('change_password')
+
+
+def more(request):
+    return HttpResponse('more text')
+
